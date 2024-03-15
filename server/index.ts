0611526102172dd6aede35ae29104613e04b286f
Henri Gaudeaux <henri.gaudeaux@sciforma.com>
2024-03-15 13:33:51 +0100

    :bug: start server...

--- server/index.ts
+++ server/index.ts
@@ -46,3 +46,11 @@ fastify.get('/memories', function (request, reply) {
   reply.send(memories)
 })
 
+fastify.listen({ port: 3000 }, function (err, address) {
+  if (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+  // Server is now listening on ${address}
+})
+
